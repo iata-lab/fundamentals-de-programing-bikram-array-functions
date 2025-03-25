a26385ba074597d4f60405727a5f19c6ad9b53e9
Pc-casa <bootcepum@gmail.com>
2025-03-25 13:25:31 +0100

    exercise resolution

---
+++ index.js
@@ -0,0 +1,31 @@
+// Coded in SPA
+
+//Arrays
+const arrayVacio = [];
+const arrayNumeros = [0,1,2,3,4,5,6,7,8,9];
+const arrayNumerosPares = [0,2,4,6,8];
+const arrayBidimensional = [[0,1,2], ["a","b","c"]];
+
+//Funciones
+function suma (a,b) {return a + b};
+suma(8,2)
+console.log(suma)
+
+function potenciacion (a,b) {return a**b};
+potenciacion(8,2)
+console.log(potenciacion)
+
+function separarPalabras(string) {return string.split(" ")}
+
+function repetirString(string,number) {return string.repeat(number)}
+
+function esPrimo(number) {
+    for (let i=0; index <= number; index++)
+    return}
+
+
+
+
+
+
+
